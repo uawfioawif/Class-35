753efae529bd0e434d200039e927ad6ef175fc7a
uawfioawif <96601910+uawfioawif@users.noreply.github.com>
2022-05-26 20:46:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -73,11 +73,11 @@ function draw() {
 //   balloon.y = height.y;
 // }
 
-// function readHeight(data){
-//   height = data.val();
-//   balloon.x = height.x;
-//   balloon.y = height.y;
-// }
+function readHeight(data){
+  height = data.val();
+  balloon.x = height.x;
+  balloon.y = height.y;
+}
 
 // function readHeight(data){
 //   height = data.val();
